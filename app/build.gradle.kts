5455822cd7101c860a2e6526bb987bdbc63a3ef9
Elchocazo <moz658@gmail.com>
2026-09-06 18:58:10 -0500

    chore(release): bump version to 2.0 (versionCode 12) for Google Play Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.escolaris.net"
     minSdk = 24
     targetSdk = 36
-    versionCode = 11
-    versionName = "1.9"
+    versionCode = 12
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
